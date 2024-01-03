fcacca7fa52c3cfa034d6bc167d58833e589f288
Mitali <mitupatil39@gmail.com>
2024-01-03 08:13:04 +0530

    mssg

---
+++ 17 Bootstrap/learn bootstrap/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+  <button type="button" class="btn btn-danger">Left</button>
+  <button type="button" class="btn btn-warning">Middle</button>
+  <button type="button" class="btn btn-success">Right</button>
+</div>
+    </div>
+  )
+}
+
+export default App
---
+++ Projects/1-todo-app-version-1/src/Components/Todo1.jsx
@@ -0,0 +1,12 @@
+function todo1(){
+    return <div className="row kg-row">
+    <div className="col-6">Buy Milk</div>
+    <div className="col-4">4/10/2023</div>
+    <div className="col-2">
+      <button type="button" className="btn btn-danger kg-button">
+        Delete
+      </button>
+    </div>
+  </div>
+}
+export default todo1;
